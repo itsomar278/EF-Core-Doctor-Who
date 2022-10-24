72c6d216491fde7a345260195abc04815a674fba
Omar Badawi <itsomar278@gmail.com>
2022-10-24 13:28:23 +0300

    fnEnemies moved to a single migration file , view from prev Task created

---
+++ DoctorWho.Db/Migrations/20221024101048_function2.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DoctorWho.Db.Migrations
+{
+    public partial class function2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+                                    CREATE FUNCTION fnEnemies (@EpisodeId INT)
+                                    RETURNS VARCHAR(MAX)
+                                    AS
+                                    BEGIN
+                                    Declare @output AS VARCHAR(MAX)
+                                        SELECT @output = CONCAT( 'Enemy No ' ,
+	                                                              CAST(ROW_NUMBER() OVER(order by E.EnemyId) AS varchar ) 
+							                                      , ' : '
+							                                     , E.EnemyName
+							                                     , ' , '
+							                                     , @output)
+	                                     FROM Enemies E
+	                                     INNER JOIN EnemyEpisodes AS EE
+	                                     ON E.EnemyId = EE.EnemyId
+	                                     WHERE @EpisodeId = EE.EpisodeId
+                                    RETURN @Output; 
+                                    END;"
+);
+        }
+
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DROP FUNCTION fnEnemies ; ");
+        }
+    }
+}
+
---
+++ DoctorWho.Db/Migrations/20221024101619_view.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DoctorWho.Db.Migrations
+{
+    public partial class view : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"CREATE VIEW [viewEpisodes] AS 
+                                   SELECT A.AuthorName AS AuthorName , 
+                                          D.DoctorName AS DoctorName , 
+	                                           dbo.fnCompanions(E.EpisodeId) AS Companions , 
+	                                           dbo.fnEnemies(E.EpisodeId) AS Enemies 
+	                                           FROM Episodes E 
+	                                           INNER JOIN Doctors D 
+	                                           ON D.DoctorId = E.DoctorID
+	                                           INNER JOIN Authors A 
+	                                           ON E.AuthorId = A.AuthorId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+           migrationBuilder.Sql("DROP VIEW[viewEpisodes];");
+        }
+    }
+}
