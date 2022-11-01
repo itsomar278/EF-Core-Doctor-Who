1f389e2fa0cc94164fbb2f0955a3b8519befb76d
Omar Badawi <itsomar278@gmail.com>
2022-11-01 23:38:32 +0200

    CUD operations added for each entity , basid update only added

--- DoctorWho/Program.cs
+++ DoctorWho/Program.cs
@@ -6,27 +6,25 @@ namespace DoctorWho
 {
     public class Program
     {
+        DoctorWhoCoreDbContext db = new DoctorWhoCoreDbContext();
         static void Main()
         {
-          DoctorWhoCoreDbContext db = new DoctorWhoCoreDbContext();
             viewEpisodes();
         }
-       public static void viewEpisodes()
+        public static void viewEpisodes()
         {
-            
-           using(var db = new DoctorWhoCoreDbContext())
+
+            using (var db = new DoctorWhoCoreDbContext())
             {
                 var episodes = db.viewEpisodes;
                 foreach (var item in episodes)
                 {
                     Console.WriteLine($"AuthorName = {item.AuthorName} , DoctorName = {item.DoctorName}" +
                                       $" , Enemies = {item.Enemies} , Companions = {item.Companions}");
-                                     
                 }
-
             }
         }
-       public static void fnCompanions(int EpisodeId)
+        public static void fnCompanions(int EpisodeId)
         {
             DoctorWhoCoreDbContext db = new DoctorWhoCoreDbContext();
 
@@ -45,7 +43,7 @@ namespace DoctorWho
                 Console.WriteLine(item);
             }
         }
-        public static void ExecueteSP ()
+        public static void ExecueteSP()
         {
             using (var db = new DoctorWhoCoreDbContext())
             {
@@ -79,7 +77,126 @@ namespace DoctorWho
                     Console.WriteLine(item.Trim());
                 }
             }
-
+        }
+        public static void AddDoctor(Doctor doctor)
+        {
+            using (var db = new DoctorWhoCoreDbContext())
+            {
+                db.Doctors.Add(doctor);
+                db.SaveChanges();
+            }
+        }
+        public static void DeleteDoctor(Doctor doctor)
+        {
+            using (var db = new DoctorWhoCoreDbContext())
+            {
+                db.Doctors.Remove(doctor);
+                db.SaveChanges();
+            }
+        }
+        public static void UpdateDoctorName(Doctor doctor, string Name)
+        {
+            using (var db = new DoctorWhoCoreDbContext())
+            {
+                doctor.DoctorName = Name;
+                db.SaveChanges();
+            }
+        }
+        public static void AddAuthor(Author author)
+        {
+            using (var db = new DoctorWhoCoreDbContext())
+            {
+                db.Authors.Add(author);
+                db.SaveChanges();
+            }
+        }
+        public static void DeleteAuthor(Author author)
+        {
+            using (var db = new DoctorWhoCoreDbContext())
+            {
+                db.Authors.Remove(author);
+                db.SaveChanges();
+            }
+        }
+        public static void UpdateAuthorName(Author author, string Name)
+        {
+            using (var db = new DoctorWhoCoreDbContext())
+            {
+                author.AuthorName = Name;
+                db.SaveChanges();
+            }
+        }
+        public static void AddCompanion(Companion companion)
+        {
+            using (var db = new DoctorWhoCoreDbContext())
+            {
+                db.Companions.Add(companion);
+                db.SaveChanges();
+            }
+        }
+        public static void DeleteCompanion(Companion companion)
+        {
+            using (var db = new DoctorWhoCoreDbContext())
+            {
+                db.Companions.Remove(companion);
+                db.SaveChanges();
+            }
+        }
+        public static void UpdateCompanionName(Companion companion, string name)
+        {
+            using (var db = new DoctorWhoCoreDbContext())
+            {
+                companion.CompanionName = name;
+                db.SaveChanges();
+            }
+        }
+        public static void AddEnemy(Enemy enemy)
+        {
+            using (var db = new DoctorWhoCoreDbContext())
+            {
+                db.Enemies.Add(enemy);
+                db.SaveChanges();
+            }
+        }
+        public static void DeleteEnemy(Enemy enemy)
+        {
+            using (var db = new DoctorWhoCoreDbContext())
+            {
+                db.Enemies.Add(enemy);
+                db.SaveChanges();
+            }
+        }
+        public static void UpdateEnemyName(Enemy Enemy, string name)
+        {
+            using (var db = new DoctorWhoCoreDbContext())
+            {
+                Enemy.EnemyName = name;
+                db.SaveChanges();
+            }
+        }
+        public static void AddEpisode(Episode episode)
+        {
+            using (var db = new DoctorWhoCoreDbContext())
+            {
+                db.Episodes.Add(episode);
+                db.SaveChanges();
+            }
+        }
+        public static void DeleteEpisode(Episode episode)
+        {
+            using (var db = new DoctorWhoCoreDbContext())
+            {
+                db.Episodes.Remove(episode);
+                db.SaveChanges();
+            }
+        }
+        public static void UpdateEpisodeTitle(Episode episode , string title)
+        {
+            using(var db= new DoctorWhoCoreDbContext())
+            {
+                episode.Title = title;
+                db.SaveChanges();
+            }
         }
     }
 }
