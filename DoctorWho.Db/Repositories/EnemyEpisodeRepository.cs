c5c4d76b872d055ef3fa65c4384ee2867730b39b
Omar Badawi <itsomar278@gmail.com>
2022-11-04 02:55:38 +0200

    Repository class added for each entity

---
+++ DoctorWho.Db/Repositories/EnemyEpisodeRepository.cs
@@ -0,0 +1,25 @@
+﻿namespace DoctorWho.Db.Repositories
+{
+    public class EnemyEpisodeRepository
+    {
+        public readonly DoctorWhoCoreDbContext _db;
+        public EnemyEpisodeRepository()
+        {
+            _db = new DoctorWhoCoreDbContext();
+        }
+        public void AddEnemyToEpisode(Enemy enemy, Episode episode)
+        {
+            if (!_db.Episodes.Contains(episode))
+                throw new ArgumentException("this episode dosent exist");
+            if (!_db.Enemies.Contains(enemy))
+                throw new ArgumentException("this enemy dosent exist");
+            EnemyEpisode enemyEpisode = new EnemyEpisode
+            {
+                EnemyId = enemy.EnemyId,
+                EpisodeId = episode.EpisodeId
+            };
+            _db.EnemyEpisodes.Add(enemyEpisode);
+            _db.SaveChanges();
+        }
+    }
+}
